9488900614db613837218377a405c905fb984b7e
NamgungJongMin <jmnamgung@gmail.com>
2024-12-19 06:26:53 +0000

    feat: zustand 설정 추가

--- src/mocks/msw.component.tsx
+++ src/mocks/msw.component.tsx
@@ -4,16 +4,18 @@ import { useEffect } from 'react';
 
 export const MswComponent = () => {
   useEffect(() => {
-    if (typeof window === 'undefined') {
-      (async () => {
-        const { server } = await import('@/mocks/server');
-        server.listen();
-      })();
-    } else {
-      (async () => {
-        const { worker } = await import('@/mocks/browser');
-        worker.start();
-      })();
+    if (process.env.NODE_ENV === 'development') {
+      if (typeof window === 'undefined') {
+        (async () => {
+          const { server } = await import('@/mocks/server');
+          server.listen();
+        })();
+      } else {
+        (async () => {
+          const { worker } = await import('@/mocks/browser');
+          worker.start();
+        })();
+      }
     }
   });
 
